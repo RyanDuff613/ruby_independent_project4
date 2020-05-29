99534723d356898acbe17e7fb0df3dfcceac4408
Ryan Duff <ryanduff613@gmail.com>
2020-05-29 11:45:56 -0700

    complete products controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+  
+  def index
+    
+  end
+
+  def new
+  
+  end
+
+  def create
+  
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+  
+  end
+
+  def destroy
+  
+  end
+
+end
