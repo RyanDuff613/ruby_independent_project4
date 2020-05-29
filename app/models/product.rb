dc48bfd156aa23d9ee0563ba51e063f1e5415477
Ryan Duff <ryanduff613@gmail.com>
2020-05-29 15:09:51 -0700

    write test for titleize callback method on Product class, write titleize callback, all tests passing

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
   validates :country_of_origin, presence: true
   validates :cost, presence: true
   validates :cost, numericality: true
+
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,3 +7,10 @@ describe Product do
   it {should validate_presence_of :cost}
   it { should validate_numericality_of(:cost)}
 end
+
+describe Product do
+  it("titleizes product name when user enters first letter as a lowercase") do
+    product = Product.create({name: "tomatoes", country_of_origin:"us", cost: "5.34"})
+    expect(product.name()).to(eq("Tomatoes"))
+  end
+end
