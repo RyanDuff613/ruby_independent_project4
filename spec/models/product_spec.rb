6a91db6801087f9e862a2e4b5546742f0c330889
Ryan Duff <ryanduff613@gmail.com>
2020-05-29 15:02:17 -0700

    write test to validate numericality of :cost for Product class, write validation, all tests passing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,5 +5,5 @@ describe Product do
   it {should validate_presence_of :name}
   it {should validate_presence_of :country_of_origin}
   it {should validate_presence_of :cost}
-  # it { validate cost ha two decimals }
+  it { should validate_numericality_of(:cost)}
 end
