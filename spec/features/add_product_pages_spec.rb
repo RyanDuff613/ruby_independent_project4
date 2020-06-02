67c7c7b9b55f7e62eb94c940aa18b5df922660c0
Ryan Duff <ryanduff613@gmail.com>
2020-06-01 23:51:34 -0700

    update buttons on products/show.html.erb

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -18,21 +18,21 @@ describe "the add product process" do
   end
 end
 
-# describe "add review process" do
-#   it "adds a new review" do
-#     visit products_path
-#     click_link 'Add new product to inventory'
-#     fill_in "Name", :with => "kale"
-#     fill_in "Country of origin", :with => "italy"
-#     fill_in "Cost", :with => 2.30
-#     click_on "Create Product"
-#     click_on "kale"
-#     click_on "Review this product"
-#     fill_in "Author", :with => "Tammy"
-#     fill_in "Rating", :with => 4
-#     fill_in "Comments", :with => "adsaf asdf asdf asdf asdf asdf asdf asdf"
-#     click_on "Create Review"
-#     expect(page).to have_content "Thanks for adding your review"
-#     expect(page).to have_content "Tammy"
-#   end
-# end
+describe "add review process" do
+  it "adds a new review" do
+    visit products_path
+    click_link 'Add new product to inventory'
+    fill_in "Name", :with => "kale"
+    fill_in "Country of origin", :with => "italy"
+    fill_in "Cost", :with => 2.30
+    click_on "Create Product"
+    click_on "Kale - $2.3"
+    click_on "Review this product"
+    fill_in "Author", :with => "Tammy"
+    fill_in "Rating", :with => 4
+    fill_in "Comments", :with => "adsaf asdf asdf asdf asdf asdf asdf asdf"
+    click_on "Create Review"
+    expect(page).to have_content "Thanks for adding your review"
+    expect(page).to have_content "Tammy"
+  end
+end
