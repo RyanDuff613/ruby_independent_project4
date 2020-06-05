4f22f1059b64771f7baf26d5ef177575a1483d20
Ryan Duff <ryanduff613@gmail.com>
2020-06-05 13:34:54 -0700

    bundle with bcrypt gem, create migration for users table, migrate to create users table

---
+++ db/migrate/20200605203055_add_users.rb
@@ -0,0 +1,9 @@
+class AddUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.column :email, :string
+      t.column :password_hash, :string
+      t.column :password_hash, :string
+    end
+  end
+end
