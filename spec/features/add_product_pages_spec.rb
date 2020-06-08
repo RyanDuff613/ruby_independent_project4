af6d3abd88570d0145a02c700f4cc0fdc7070bb2
Ryan Duff <ryanduff613@gmail.com>
2020-06-08 02:01:41 -0700

    create and pass test for rejecting non-admin deletion of review. 98% test coverage

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -54,7 +54,6 @@ describe "sign in as admin process" do
 end
 
 describe "the add product process" do
-
   it"adds a new product" do
     visit products_path 
     click_link "Sign up"
@@ -69,6 +68,12 @@ describe "the add product process" do
     fill_in "Cost", :with => 2.30
     click_on "Create Product"
     expect(page).to have_content 'Product Successfully Added to Inventory'
+  end
+
+  it"rejects non-admin attempting to add product" do
+    visit products_path 
+    click_link 'Add new product to inventory'
+    expect(page).to have_content 'Restricted. Admin only'
   end
 
   it "gives error when no name is entered" do
@@ -279,4 +284,32 @@ describe "delete review process" do
     click_link "Tammy"
     expect(page).to have_content "Impedit sed ea incidunt tenetur placeat est labore quia ut e."
   end
+
+  it "rejects non-admin attempt to delete review" do
+    visit products_path
+    click_link "Sign up"
+    fill_in "Email", :with => "meme.com"
+    fill_in "Password", :with => "123abc"
+    fill_in "Password confirmation", :with => "123abc"
+    click_button "Register as Admin"
+    visit products_path
+    click_link 'Add new product to inventory'
+    fill_in "Name", :with => "kale"
+    fill_in "Country of origin", :with => "italy"
+    fill_in "Cost", :with => 2.31
+    click_on "Create Product"
+    click_link "Sign out"
+    visit products_path
+    click_link "Kale - $2.31"
+    click_link "Add Review for this Product"
+    fill_in "Author", :with => "Tammy"
+    fill_in "Rating", :with => 4 
+    fill_in "Comments", :with => "Impedit sed ea incidunt tenetur placeat est labore quia ut e."
+    click_on "Create Review"
+    click_link "Tammy"
+    click_button "Delete this review"
+    expect(page).to have_content "Restricted. Admin only"
+  end
+
+
 end
