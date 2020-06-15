83ce824c1cae45fb89e22d4598f69cdaf5756740
Ryan Duff <ryanduff613@gmail.com>
2020-06-15 01:24:26 -0700

    complete scope for most reviewed product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   validates :country_of_origin, presence: true
   validates :cost, presence: true
   validates :cost, numericality: true
-  scope :newest_3, -> {order(created_at: :desc).limit(3)}
+  scope :newest_3, -> {order(created_at: :desc).limit(1)}
   scope :from_mexico, -> {where(country_of_origin: "Mexico")}
   scope :most_reviewed, -> {(
     # select ("products.name, product.name, count(reviews.id) as total_reviews")
@@ -14,7 +14,7 @@ class Product < ApplicationRecord
     # .limit(1)
     # ------ Same statement, for use in Rails console
     #  select("products.id, count(reviews.id) as total_reviews").joins(:reviews).group("products.id").order("total_reviews DESC").limit(1)
-    select("products.id, product.name, count(reviews.id) as total_reviews")
+    select("products.id, products.name, count(reviews.id) as total_reviews")
     .joins(:reviews)
     .group("products.id")
     .order("total_reviews DESC")
