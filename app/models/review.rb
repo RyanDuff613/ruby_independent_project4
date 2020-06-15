e3b7debdf1fe60b9159eb82c9ce037c0986f11ec
Ryan Duff <ryanduff613@gmail.com>
2020-06-15 02:00:01 -0700

    add error messages for adding reviews, update validations to reduce redundancy in error messages

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,15 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
-  validates :content_body, presence: true
   validates :rating, presence: true
-  validates_length_of :content_body, maximum: 250
-  validates_length_of :content_body, minimum: 50
-  validates :rating, numericality: {only_integer: true}
-  validates :rating, numericality: {greater_than: 0}
-  validates :rating, numericality: {less_than: 6}
-  
-
+  validates :rating, numericality: {only_integer: true}, numericality: {greater_than: 0}, numericality: {less_than: 6}
+  validates :content_body, presence: true
+  validates_length_of :content_body, maximum: 250, minimum: 50
+ 
   before_save(:titleize_author_name)
 
   private
